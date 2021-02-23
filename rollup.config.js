5e8c56892e43138b6d05d29c3d9eed441dce27d7
Dave Batiste <dave.batiste@d2l.com>
2021-02-23 17:05:54 -0500

    Add d2l-htmleditor to the hello-world FRA.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,13 @@
+import copy from 'rollup-plugin-copy';
 import merge from 'deepmerge';
 import { createSpaConfig } from '@open-wc/building-rollup';
+
+// NOTE: Ideally these should all be dynamically imported by apps. These
+// are included because tinyMCE dynamically requests various files that
+// rollout can't discover.
+const staticFiles = [
+	'node_modules/@brightspace-ui/htmleditor/tinymce/**'
+];
 
 const baseConfig = createSpaConfig({
 	developmentMode: false,
@@ -7,5 +15,13 @@ const baseConfig = createSpaConfig({
 });
 
 export default merge(baseConfig, {
-	input: './src/index.html'
+	input: './src/index.html',
+	plugins: [
+		copy({
+			targets: staticFiles.map((f) => {
+				return {src: f, dest: 'dist/node_modules'};
+			}),
+			flatten: false
+		})
+	]
 });
--- src/hello-world-app.js
+++ src/hello-world-app.js
@@ -2,6 +2,7 @@ import '@brightspace-ui/core/components/button/button.js';
 import '@brightspace-ui/core/components/demo/code-view.js';
 import '@brightspace-ui/core/components/dialog/dialog-confirm.js';
 import '@brightspace-ui/core/components/typography/typography.js';
+import '@brightspace-ui/htmleditor/htmleditor.js';
 import { LitElement, html, css } from 'lit-element';
 import { d2lfetch } from 'd2l-fetch/src/index.js';
 import { fetchAuthFramed } from 'd2l-fetch-auth';
@@ -71,22 +72,23 @@ export class HelloWorldApp extends LitElement {
 		const code = this.data ? html`<d2l-code-view language="javascript" hide-language>${JSON.stringify(this.data, null, '    ')}</d2l-code-view>` : null;
 		const whoami = this.whoami ? html`<d2l-code-view language="javascript" hide-language>${JSON.stringify(this.whoami, null, '    ')}</d2l-code-view>` : null;
 		return html`
+
 			<h1>Hello World</h1>
-			<p>
-				This is a simple free-range application demonstrating an
-				IFRAME-based app.
-			</p>
+			<p>This is a simple free-range application demonstrating an IFRAME-based app.</p>
+
 			<h2>Dialogs</h2>
 			<d2l-dialog-confirm title-text="Confirm Dialog" text="Are you sure?">
 				<d2l-button slot="footer" primary data-dialog-action="yes">Yes</d2l-button>
 				<d2l-button slot="footer" data-dialog-action>No</d2l-button>
 			</d2l-dialog-confirm>
 			<d2l-button @click="${this._openDialog}">Open Dialog</d2l-button>
+
 			<h2>Runtime data provided via ifrau:</h2>
 			${code}
 			<h2>Who Am I?</h2>
 			<p>Results of a <strong>whoami</strong> Valence API call:</p>
 			${whoami}
+
 			<h2>Page Title Sync</h2>
 			<p>
 				The IFRAME's page title and the title of the outer page
@@ -96,11 +98,15 @@ export class HelloWorldApp extends LitElement {
 				page title
 				<input type="text" onchange="document.title=value;" value="hello world" />
 			</label>
+
 			<h2>Branding Sync</h2>
 			<p>Organization configurable branding-related CSS variables are synchronized.</p>
 			<div class="branding-primary">
 				Primary
 			</div>
+
+			<h2>HtmlEditor</h2>
+			<d2l-htmleditor></d2l-htmleditor>
 		`;
 	}
 
